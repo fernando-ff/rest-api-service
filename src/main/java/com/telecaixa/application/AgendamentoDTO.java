526e411e1c5e1c7c0165ddeca8993c1b6c0fe1e0
Fernando Ferreira <fferreiralf.1@gmail.com>
2026-07-21 18:56:15 -0300

    fix: AgendamentoDTO register for reflection

--- src/main/java/com/telecaixa/application/AgendamentoDTO.java
+++ src/main/java/com/telecaixa/application/AgendamentoDTO.java
@@ -1,5 +1,8 @@
 package com.telecaixa.application;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public class AgendamentoDTO {
 
     private String data;
